82993d9eb5ea2fd52f74f4263360cb4f309b2d19
emsknock <25252659+emsknock@users.noreply.github.com>
2023-10-25 16:00:51 +0300

    List countries alongside flags

---
+++ src/data/countries.ts
@@ -0,0 +1,35 @@
+// prettier-ignore
+export const COUNTRIES = [
+    "Austria",   "Belgium",
+    "Bulgaria",  "Croatia",
+    "Cyprus",    "Czechia",
+    "Denmark",   "Estonia",
+    "Finland",   "France",
+    "Germany",   "Greece",
+    "Hungary",   "Ireland",
+    "Italy",     "Latvia",
+    "Lithuania", "Luxembourg",
+    "Malta",     "Netherlands",
+    "Poland",    "Portugal",
+    "Romania",   "Slovakia",
+    "Slovenia",  "Spain",
+    "Sweden",    "United Kingdom"
+];
+
+// prettier-ignore
+export const FLAGS: Record<string, string> = {
+    "Austria": "🇦🇺",   "Belgium": "🇧🇪",
+    "Bulgaria": "🇧🇬",  "Croatia": "🇭🇷",
+    "Cyprus": "🇨🇾",    "Czechia": "🇨🇿",
+    "Denmark": "🇩🇰",   "Estonia": "🇪🇪",
+    "Finland": "🇫🇮",   "France": "🇫🇷",
+    "Germany": "🇩🇪",   "Greece": "🇬🇷",
+    "Hungary": "🇭🇺",   "Ireland": "🇮🇪",
+    "Italy": "🇮🇹",     "Latvia": "🇱🇻",
+    "Lithuania": "🇱🇹", "Luxembourg": "🇱🇺",
+    "Malta": "🇲🇹",     "Netherlands": "🇳🇱",
+    "Poland": "🇵🇱",    "Portugal": "🇵🇹",
+    "Romania": "🇷🇴",   "Slovakia": "🇸🇰",
+    "Slovenia": "🇸🇮",  "Spain": "🇪🇸",
+    "Sweden": "🇸🇪",    "United Kingdom": "🇬🇧",
+};
